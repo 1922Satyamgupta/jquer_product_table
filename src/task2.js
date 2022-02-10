a0a7e60c6d1f3ea3e0f0f8f646a11da72509aeef
satyam <raj@gmail.com>
2022-02-10 17:58:19 +0530

    first commit

---
+++ src/task2.js
@@ -0,0 +1,44 @@
+var products = [{
+    "id": "100",
+    "name": "iPhone 4S",
+    "brand": "Apple",
+    "os": "iOS"
+},
+{
+    "id": "101",
+    "name": "Moto X",
+    "brand": "Motorola",
+    "os": "Android"	
+},
+{
+    "id": "102",
+    "name": "iPhone 6",
+    "brand": "Apple",
+    "os": "iOS"
+},
+{
+    "id": "103",
+    "name": "Samsung Galaxy S",
+    "brand": "Samsung",
+    "os": "Android"
+},
+{
+    "id": "104",
+    "name": "Google Nexus",
+    "brand": "ASUS",
+    "os": "Android"
+},
+{
+    "id": "105",
+    "name": "Surface",
+    "brand": "Microsoft",
+    "os": "Windows"
+}];
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
